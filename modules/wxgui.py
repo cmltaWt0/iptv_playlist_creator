cddac3dc64cab009f9aab5fcf49a33f4331cbb81
Maksim Sokolskiy <maksim.sokolskiy@gmail.com>
2011-12-26 17:33:35 +0200

    Changes in wxgui

--- modules/wxgui.py
+++ modules/wxgui.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from wx import App, Frame, StaticText, Button, EVT_BUTTON
-
+from wx import *
 '''
 Created on 23.12.2011
 wx Class for main frame
@@ -17,24 +16,58 @@ class MainFrame(Frame):
         Constructor
         '''
         Frame.__init__(self, parent, -1, 'Iptv_creator')
-        self.text = StaticText(self, -1,'0', (120, 40))
-        button1 = Button(self, -1, '+', (10, 10))
-        button2 = Button(self, -1, '-', (10, 60))
         
-        self.Bind(EVT_BUTTON, self.OnPlus, id=button1.GetId())
-        self.Bind(EVT_BUTTON, self.OnMinus, id=button2.GetId())
+        self.InitUI()
+        self.text = StaticText(self, -1,'Пакеты.', (10, 40))
+        self.cb1 = CheckBox(self, -1, 'Социальный',(10, 90))
+        self.cb1.SetValue(False)
+        self.text = StaticText(self, -1,'колличество: ', (10, 130))
+        TextCtrl(self, pos=(120, 120), size=(40, 30))
         
+        self.cb1 = CheckBox(self, -1, 'Базовый',(10, 90))
+        self.cb1.SetValue(False)
+        self.text = StaticText(self, -1,'колличество: ', (10, 130))
+        TextCtrl(self, pos=(120, 120), size=(40, 30))
+        
+        self.cb1 = CheckBox(self, -1, 'Полный',(10, 90))
+        self.cb1.SetValue(False)
+        self.text = StaticText(self, -1,'колличество: ', (10, 130))
+        TextCtrl(self, pos=(120, 120), size=(40, 30))
+        
+        button = Button(self, -1, '+', (10, 10))
+        
+        
+        TextCtrl(self, pos=(300, 400), size=(250, 150))
+        
+        self.Bind(EVT_BUTTON, self.Gen, id=button.GetId())
+        
+        EVT_CHECKBOX(self, self.cb1.GetId(), self.SetCb1)
+        
+        self.SetSize((800, 600))
         self.Centre()
         
-    def OnPlus(self, event):
-        value = int(self.text.GetLabel())
-        value = value + 1
-        self.text.SetLabel(str(value))
+    def InitUI(self):    
+
+        menubar = MenuBar()
+        fileMenu = Menu()
+        fitem = fileMenu.Append(ID_EXIT, 'Quit', 'Quit application')
+        menubar.Append(fileMenu, '&File')
         
-    def OnMinus(self, event):
-        value = int(self.text.GetLabel())
-        value = value - 1
-        self.text.SetLabel(str(value))
+        self.SetMenuBar(menubar)
+        
+        self.Bind(EVT_MENU, self.OnQuit, fitem)
+        
+    def SetCb1(self, event):
+        if self.cb1.GetValue():
+            self.setcb1 = True
+        else: self.setcb1 = False
+        
+    def OnQuit(self, e):
+        self.Close()
+        
+    def Gen(self):
+        pass
+        
         
         
 class MainApp(App):
