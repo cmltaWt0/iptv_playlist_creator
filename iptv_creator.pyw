8c929f07e63fdb3e836e2d07fe4955b5c16e82d8
Maksim Sokolsky <misokolsky@gmail.com>
2012-03-07 16:55:26 +0200

    Add base and full package to playlist with selection of cities

--- iptv_creator.pyw
+++ iptv_creator.pyw
@@ -14,6 +14,22 @@ Creating IPTV playlist from EXCEL Servise PLAN.
 '''
          
 if __name__ == '__main__':
-    playlist = Playlist(sys.argv[1])
+
+    if sys.argv[2] == 'kh' or sys.argv[2] == 'pt':
+        soc_start = 7
+        soc_end = 56
+        base_start = 59
+        base_end = 78
+        full_start = 81
+        full_end = 110
+    if sys.argv[2] == 'dn':
+        soc_start = 7 
+        soc_end = 41
+        base_start = 44 
+        base_end = 67 
+        full_start = 67 
+        full_end = 66 #in Playlist "items_full = list(range(full_start, full_end + 1))", so result list should be empty
+
+    playlist = Playlist(sys.argv[1], soc_start, soc_end, base_start, base_end, full_start, full_end)
     playlist.parse_xls()
     playlist.save_playlist('tmp/tmp_file.txt')
--- modules/playlist.py
+++ modules/playlist.py
@@ -14,31 +14,35 @@ class Playlist(object):
     '''
 
 
-    def __init__(self, open_file):
+    def __init__(self, open_file, soc_start = 7, soc_end = 56, base_start = 59, base_end = 78, full_start = 81, full_end = 110):
         '''
         Constructor
         '''
         self.sheet0 = open_workbook(open_file).sheet_by_index(0)
+        self.social = list(range(int(soc_start), int(soc_end) + 1))
+        self.base = list(range(int(base_start), int(base_end) + 1))
+        self.full = list(range(int(full_start), int(full_end) + 1))
         
     def save_playlist(self, path):
         
         f = codecs.open(path, "w", "utf-8")
         f.write(self.playlist)
         f.close()
+        print(self.playlist)
+        print('Плейлист успешно сохранен в файл tmp/tmp.txt.\n')
               
         
     def parse_xls(self):
         self.playlist = '#EXTM3U\n\n'
-        items = []
-        social = list(range(7,57))
-        base = list(range(59,79))
-        full = list(range(81,111))
+        items_social = []
+        items_base = []
+        items_full = []
 
-        [items.append(i) for i in social]
-        [items.append(i) for i in base]
-        [items.append(i) for i in full]
+        [items_social.append(i) for i in self.social]
+        [items_base.append(i) for i in self.base]
+        [items_full.append(i) for i in self.full]
 
-        for i in items:
+        for i in items_social:
             item = self.sheet0.row_values(i-1, 0, 3)
             if item[1] < 10:
                 self.playlist += '#EXTINF:0,00'
@@ -47,5 +51,21 @@ class Playlist(object):
             else:
                 self.playlist += '#EXTINF:0,0'
             self.playlist += str(item[1]) + ' ' + '[' + 'SOCIAL' + ']' + ' ' + item[0] + '\n' + 'udp://@' + str(item[2]) + ':5004' + '\n\n'
-        
-    
+        for i in items_base:
+            item = self.sheet0.row_values(i-1, 0, 3)
+            if item[1] < 10:
+                self.playlist += '#EXTINF:0,00'
+            elif item[1] > 99:
+                self.playlist += '#EXTINF:0,'
+            else:
+                self.playlist += '#EXTINF:0,0'
+            self.playlist += str(item[1]) + ' ' + '[' + 'BASE' + ']' + ' ' + item[0] + '\n' + 'udp://@' + str(item[2]) + ':5004' + '\n\n'
+        for i in items_full:
+            item = self.sheet0.row_values(i-1, 0, 3)
+            if item[1] < 10:
+                self.playlist += '#EXTINF:0,00'
+            elif item[1] > 99:
+                self.playlist += '#EXTINF:0,'
+            else:
+                self.playlist += '#EXTINF:0,0'
+            self.playlist += str(item[1]) + ' ' + '[' + 'FULL' + ']' + ' ' + item[0] + '\n' + 'udp://@' + str(item[2]) + ':5004' + '\n\n'
